fdb263cef68d89e7bdfc9aabd2d420ab5d9c8227
Callum Chace-Macki <mauveshishito14@gmail.com>
2023-01-26 19:18:25 -0800

    Add venv and aruco examples

---
+++ aruco_webstreaming_2.py
@@ -0,0 +1,209 @@
+# https://pyimagesearch.com/2019/09/02/opencv-stream-video-to-web-browser-html-page/
+
+# import the necessary packages
+from singlemotiondetector import SingleMotionDetector
+from imutils.video import VideoStream
+from flask import Response
+from flask import Flask
+from flask import render_template
+import threading
+import argparse
+import datetime
+import imutils
+import time
+import cv2
+import sys
+
+# define names of each possible ArUco tag OpenCV supports
+ARUCO_DICT = {
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+# initialize the output frame and a lock used to ensure thread-safe
+# exchanges of the output frames (useful when multiple browsers/tabs
+# are viewing the stream)
+outputFrame = None
+lock = threading.Lock()
+# initialize a flask object
+app = Flask(__name__)
+# initialize the video stream and allow the camera sensor to
+# warmup
+#vs = VideoStream(usePiCamera=1).start()
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+@app.route("/")
+def index():
+	# return the rendered template
+	return render_template("index.html")
+
+def detect_motion(frameCount):
+	# grab global references to the video stream, output frame, and
+	# lock variables
+	global vs, outputFrame, lock
+	# initialize the motion detector and the total number of frames
+	# read thus far
+	md = SingleMotionDetector(accumWeight=0.1)
+	total = 0
+
+	# loop over frames from the video stream
+	while True:
+		# read the next frame from the video stream, resize it,
+		# convert the frame to grayscale, and blur it
+		frame = vs.read()
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		gray = cv2.GaussianBlur(gray, (7, 7), 0)
+		# grab the current timestamp and draw it on the frame
+		timestamp = datetime.datetime.now()
+		cv2.putText(frame, timestamp.strftime(
+			"%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+		# Perform the tag detection and overlay bounding boxes and text
+		frame_detect = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		# detect ArUco markers in the input frame
+		(corners, ids, rejected) = cv2.aruco.detectMarkers(frame_detect, arucoDict, parameters=arucoParams)
+
+		# verify *at least* one ArUco marker was detected
+		if len(corners) > 0:
+			# flatten the ArUco IDs list
+			ids = ids.flatten()
+
+			# loop over the detected ArUCo corners
+			for (markerCorner, markerID) in zip(corners, ids):
+				# extract the marker corners (which are always returned
+				# in top-left, top-right, bottom-right, and bottom-left
+				# order)
+				corners = markerCorner.reshape((4, 2))
+				(topLeft, topRight, bottomRight, bottomLeft) = corners
+				# convert each of the (x, y)-coordinate pairs to integers
+				topRight = (int(topRight[0]), int(topRight[1]))
+				bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+				bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+				topLeft = (int(topLeft[0]), int(topLeft[1]))
+				# draw the bounding box of the ArUCo detection
+				cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
+				cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
+				cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
+				cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
+				# compute and draw the center (x, y)-coordinates of the
+				# ArUco marker
+				cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+				cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+				cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
+				# draw the ArUco marker ID on the frame
+				cv2.putText(frame, str(markerID),
+					(topLeft[0], topLeft[1] - 15),
+					cv2.FONT_HERSHEY_SIMPLEX,
+					0.5, (0, 255, 0), 2)
+
+		# if the total number of frames has reached a sufficient
+		# number to construct a reasonable background model, then
+		# continue to process the frame
+
+		# frame = imutils.resize(frame, width=800)
+
+		if total > frameCount:
+			# detect motion in the image
+			motion = md.detect(gray)
+			# check to see if motion was found in the frame
+			if motion is not None:
+				# unpack the tuple and draw the box surrounding the
+				# "motion area" on the output frame
+				(thresh, (minX, minY, maxX, maxY)) = motion
+				cv2.rectangle(frame, (minX, minY), (maxX, maxY),
+					(0, 0, 255), 2)
+		
+		# update the background model and increment the total number
+		# of frames read thus far
+		md.update(gray)
+		total += 1
+		# acquire the lock, set the output frame, and release the
+		# lock
+		with lock:
+			outputFrame = frame.copy()
+
+def generate():
+	# grab global references to the output frame and lock variables
+	global outputFrame, lock
+	# loop over frames from the output stream
+	while True:
+		# wait until the lock is acquired
+		with lock:
+			# check if the output frame is available, otherwise skip
+			# the iteration of the loop
+			if outputFrame is None:
+				continue
+			# encode the frame in JPEG format
+			(flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+			# ensure the frame was successfully encoded
+			if not flag:
+				continue
+		# yield the output frame in the byte format
+		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+			bytearray(encodedImage) + b'\r\n')
+
+@app.route("/video_feed")
+def video_feed():
+	# return the response generated along with the specific media
+	# type (mime type)
+	return Response(generate(),
+		mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+# check to see if this is the main thread of execution
+if __name__ == '__main__':
+	# construct the argument parser and parse command line arguments
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-i", "--ip", type=str, required=True,
+		help="ip address of the device")
+	ap.add_argument("-o", "--port", type=int, required=True,
+		help="ephemeral port number of the server (1024 to 65535)")
+	ap.add_argument("-f", "--frame-count", type=int, default=32,
+		help="# of frames used to construct the background model")
+	ap.add_argument("-t", "--type", type=str,
+		default="DICT_APRILTAG_16h5",
+		help="type of ArUCo tag to detect")
+	args = vars(ap.parse_args())
+
+	# verify that the supplied ArUCo tag exists and is supported by OpenCV
+	if ARUCO_DICT.get(args["type"], None) is None:
+		print("[INFO] ArUCo tag of '{}' is not supported".format(
+			args["type"]))
+		sys.exit(0)
+
+	# load the ArUCo dictionary and grab the ArUCo parameters
+	print("[INFO] detecting '{}' tags...".format(args["type"]))
+	arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[args["type"]])
+	arucoParams = cv2.aruco.DetectorParameters_create()
+
+	# start a thread that will perform motion detection
+	t = threading.Thread(target=detect_motion, args=(
+		args["frame_count"],))
+	t.daemon = True
+	t.start()
+	# start the flask app
+	app.run(host=args["ip"], port=args["port"], debug=True,
+		threaded=True, use_reloader=False)
+
+# release the video stream pointer
+vs.stop()
